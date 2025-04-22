d209114295d25f7a39ebbb43054873aed269e8c7
chronosinfo <chronosinfo@chronosinfo.com.br>
2025-04-22 12:32:18 -0300

    init project

---
+++ src/test/java/com/sensor/device/management/DeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sensor.device.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
