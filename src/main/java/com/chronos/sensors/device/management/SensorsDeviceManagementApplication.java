db325676c1bafe9430d9fd69b9953dde51d5e437
chronosinfo <chronosinfo@chronosinfo.com.br>
2025-04-23 11:59:22 -0300

    init project

---
+++ src/main/java/com/chronos/sensors/device/management/SensorsDeviceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.chronos.sensors.device.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorsDeviceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorsDeviceManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chronos/sensors/device/management/SensorsDeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronos.sensors.device.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorsDeviceManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
